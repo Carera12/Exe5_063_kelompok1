97a0077f808678e726443ba71b4bc32e85ed7e1e
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-12-19 16:04:47 +0700

    add class node

--- Exercise5/Program.cs
+++ Exercise5/Program.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Exercise5
 {
-    internal class Program
+    public class QueueLinkedList
+    {
+        class Node
+        {
+            public int SC;
+            public Node next;
+            public Node(int sc)
+            {
+                SC = sc;
+            }
+        }
+        
+
+    }
+    class Program
     {
         static void Main(string[] args)
         {
